bc981674198f4c254f5ca847b7cc8e3126b2020e
kafree <kirillov.bot.daniil@yandex.ru>
2019-12-02 08:11:33 +0300

    Функция, добавляющая новый объект

--- metric_reader_writer.py
+++ metric_reader_writer.py
@@ -2,7 +2,7 @@
 
 
 def read_metrics(path):
-    # Возвращает данные в виде: {ключ:[список метрик]}
+    # Возвращает данные в виде: {объект:[список метрик]}
     with open(path, encoding='UTF-8') as file:
         meta = file.readline().strip().split('|')  # сохраняем строку с метаданными
         data = {line.split(':')[0]: line.split(':')[1].strip().split('|') for line in file.readlines()}
@@ -10,7 +10,7 @@ def read_metrics(path):
 
 
 def add_new_metric(path, label, metric):
-    # Дозаписывет метрику в файл с данными. label - метка метрики, metric - {ключ:значение метрики}
+    # Дозаписывет метрику в файл с данными. label - метка метрики, metric - {объект:значение метрики}
     meta, data = read_metrics(path)
     with open(path, 'w', encoding='UTF-8') as file:
         file.write('|'.join(meta) + '|' + label + '\n')
@@ -19,14 +19,22 @@ def add_new_metric(path, label, metric):
 
 
 def change_existed_metric(path, label, metric):
+    # Меняет указанную метрику в файле с данными. label - метка метрики, metric - {объект:значение метрики}
     meta, data = read_metrics(path)
     with open(path, 'w', encoding='UTF-8') as file:
-        print(meta)
         i = meta.index(label)
         file.write('|'.join(meta) + '\n')
         for key, value in data.items():
             value[i] = metric[key]
             file.write(key + ':' + '|'.join(value) + '\n')
+
+
+def add_new_object(path, key, metrics):
+    # Добавляет новый объект в конец файла с данными. key - имя объекта, metrics - {метка метрики:значение метрики}
+    with open(path, 'a', encoding='UTF-8') as file:
+        meta = file.readline().strip().split('|')
+        line = [metrics[label] if label in metrics else 0 for label in meta]
+        file.write(key + ':' + '|'.join(line) + '\n')
 
 
 change_existed_metric('test.txt', 'tst2', {'test1': 'four', 'test2': 'cuatro'})
