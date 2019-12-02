528aaa6b92e62efa7074ee1b1e015ca054dd378b
kafree <kirillov.bot.daniil@yandex.ru>
2019-12-02 07:52:02 +0300

    Две предыдущие функции теперь ещё и работают)

--- metric_reader_writer.py
+++ metric_reader_writer.py
@@ -3,7 +3,7 @@
 
 def read_metrics(path):
     # Возвращает данные в виде: {ключ:[список метрик]}
-    with open(path) as file:
+    with open(path, encoding='UTF-8') as file:
         meta = file.readline().strip().split('|')  # сохраняем строку с метаданными
         data = {line.split(':')[0]: line.split(':')[1].strip().split('|') for line in file.readlines()}
         return meta, data
@@ -12,7 +12,7 @@ def read_metrics(path):
 def add_new_metric(path, label, metric):
     # Дозаписывет метрику в файл с данными. label - метка метрики, metric - {ключ:значение метрики}
     meta, data = read_metrics(path)
-    with open(path, 'w') as file:
+    with open(path, 'w', encoding='UTF-8') as file:
         file.write('|'.join(meta) + '|' + label + '\n')
         for key, value in data.items():
             file.write(key + ':' + '|'.join(value) + '|' + metric[key] + '\n')
@@ -20,10 +20,13 @@ def add_new_metric(path, label, metric):
 
 def change_existed_metric(path, label, metric):
     meta, data = read_metrics(path)
-    with open(path, 'w') as file:
+    with open(path, 'w', encoding='UTF-8') as file:
+        print(meta)
         i = meta.index(label)
+        file.write('|'.join(meta) + '\n')
         for key, value in data.items():
             value[i] = metric[key]
             file.write(key + ':' + '|'.join(value) + '\n')
 
-add_new_metric('test.txt', 'tst', {'test1': 'four', 'test2': 'cuatro'})
+
+change_existed_metric('test.txt', 'tst2', {'test1': 'four', 'test2': 'cuatro'})
