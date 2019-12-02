e939e54fd50ddfe6e15c8c7b7c978991d076dd39
kafree <kirillov.bot.daniil@yandex.ru>
2019-12-02 08:43:50 +0300

    Функция, изменяющая существующий объект

--- metric_reader_writer.py
+++ metric_reader_writer.py
@@ -31,10 +31,22 @@ def change_existed_metric(path, label, metric):
 
 def add_new_object(path, key, metrics):
     # Добавляет новый объект в конец файла с данными. key - имя объекта, metrics - {метка метрики:значение метрики}
-    with open(path, 'a', encoding='UTF-8') as file:
+    # Если метрика не посчитана, пишет 0.
+    with open(path, 'r+', encoding='UTF-8') as file:
         meta = file.readline().strip().split('|')
-        line = [metrics[label] if label in metrics else 0 for label in meta]
+        line = [metrics[label] if label in metrics else str(0) for label in meta]
         file.write(key + ':' + '|'.join(line) + '\n')
 
 
-change_existed_metric('test.txt', 'tst2', {'test1': 'four', 'test2': 'cuatro'})
+def change_existed_object(path, key, metrics):
+    # Изменяет существующий объект в файле с данными. key - имя объекта, metrics - {метка метрики:значение метрики}
+    # Если метрика не посчитана, оставляет старое значение.
+    meta, data = read_metrics(path)
+    with open(path, 'w', encoding='UTF-8') as file:
+        file.write('|'.join(meta) + '\n')
+        data[key] = [metrics[label] if label in metrics else data[key][meta.index(label)] for label in meta]
+        for key, value in data.items():
+            file.write(key + ':' + '|'.join(value) + '\n')
+
+
+change_existed_object('test.txt', 'test1', {'tst1': 'один', 'tst3': 'три'})
